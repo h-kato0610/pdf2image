31c82ba31b59bf9d4db0ac73f76a8145850e8374
h.kato0610 <h.kato0610@gmail.com>
2025-03-24 01:19:56 +0900

    Refactor: コメントを修正

--- pdf2img.py
+++ pdf2img.py
@@ -2,23 +2,24 @@ import sys
 from pathlib import Path
 from pdf2image import convert_from_path
 
-# is input pdf name?
+# Is input PDF name?
 try:
     pdf_name = sys.argv[1]
 except:
     print('''
-        pdf name not found
-        ex. python pdf2img.py hoge.pdf
+        error: PDFfile NameNotFound
+        ex. 
+        python pdf2img.py hoge.pdf
     ''')
     sys.exit(1)
 
 pdf_path = Path(f'./{pdf_name}')
 img_path = Path('./output')
 
-# make output dir
+# Make output dir
 if img_path.exists() == False:
     img_path.mkdir()
 
-# convert
+# Convert
 convert_from_path(pdf_path, output_folder=img_path, fmt='jpeg', output_file=pdf_path.stem)
 
